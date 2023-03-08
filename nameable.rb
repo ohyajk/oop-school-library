7cfcdd15d596720d0950b534033cdec663091e1e
ohyajk <ohyajk@gmail.com>
2023-03-08 18:52:33 +0530

    end of line fix

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
-    def correct_name
-      "#{self.class} has not implemented method '#{__method__}'"
-    end
+  def correct_name
+    "#{self.class} has not implemented method '#{__method__}'"
   end
+end
